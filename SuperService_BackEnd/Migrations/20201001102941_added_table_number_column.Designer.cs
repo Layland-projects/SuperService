45e7257b6e22c971b31a600f8ea4e92072baf327
Daniel Layland <laylandwork@gmail.com>
2020-10-01 11:30:16 +0100

    Update table model to include table number

--- SuperService_BackEnd/Migrations/20201001102941_added_table_number_column.Designer.cs
+++ SuperService_BackEnd/Migrations/20201001102941_added_table_number_column.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SuperService_BackEnd;
 
 namespace SuperService_BackEnd.Migrations
 {
     [DbContext(typeof(SuperServiceContext))]
-    partial class SuperServiceContextModelSnapshot : ModelSnapshot
+    [Migration("20201001102941_added_table_number_column")]
+    partial class added_table_number_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
