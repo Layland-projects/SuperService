c30bf16f3d9e4eb304ee5d3f11c6c7d5079d1062
Daniel Layland <laylandwork@gmail.com>
2020-10-06 13:59:38 +0100

    Added a unit test for UserHelper.GetAllUsers()

---
+++ SuperService_Tests/UserHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using SuperService_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperService_BusinessLayer.Tests
+{
+    [TestFixture]
+    public class UserHelperTests
+    {
+        [Test]
+        public void GetAllUsersTest()
+        {
+            UserHelper uHelper = new UserHelper();
+            Assert.IsTrue(uHelper.GetAllUsers().Count() > 0);
+        }
+    }
+}
